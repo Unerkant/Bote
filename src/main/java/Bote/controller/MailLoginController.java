23127985926f609d8ef6e74eeeebc1f66c0ece14
Unerkant <freiwifi@gmail.com>
2022-08-24 16:06:38 +0200

    1. Umstellung von H2 Database auf MySql(phpMyAdmin) url: jdbc:mysql://localhost:8889/globalBote von MAMP 2. hibernate umstellungen waren nicht notwendig, alles functionier 100%

--- src/main/java/Bote/controller/MailLoginController.java
+++ src/main/java/Bote/controller/MailLoginController.java
@@ -7,11 +7,11 @@ import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.Locale;
 
